2311d93854075c75b653913fa0d30bbe0ef1793a
KGuseva <marzia240015@gmail.com>
2025-09-03 13:16:55 +0300

    feat: update component ThemeSwitcher

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import cls from "./ThemeSwitcher.module.scss";
+import { classNames } from 'shared/lib/classNames/classNames';
 import { Button } from 'shared/ui/Button/Button';
+import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext';
+import iconLight from "../../../shared/assets/icons/studio-theme-icon-light.png";
+import iconDark from "../../../shared/assets/icons/studio-theme-icon-dark.png";
+import cls from "./ThemeSwitcher.module.scss";
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -12,11 +15,17 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
   const { className } = props;
   const {t, i18n} = useTranslation();
-  const {handleTheme} = useTheme();
+  const {theme, handleTheme} = useTheme();
 
   return (
     <>
-    <Button onClick={handleTheme} className={classNames(cls.themeSwitcher, {}, [className])}>{t('theme')}</Button>
+      <Button onClick={handleTheme} className={classNames(cls.themeSwitcher, {}, [className])}>
+        <img 
+          src={theme === Theme.LIGHT ? iconLight : iconDark}
+          className={classNames(cls.iconTheme, {}, [])}
+          alt={theme === Theme.LIGHT ? t('themeLight') : t('themeDark')}
+        />
+      </Button>
     </>
   )
 }
